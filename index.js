d808e9d771e3d3ae3082c4d56a41aa808778d43d
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-06-25 17:24:47 +0600

    post a review get review option

--- index.js
+++ index.js
@@ -1,159 +1,187 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port = process.env.SERVER_PORT || 5000
-const uri = process.env.MONGODB_URI
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.SERVER_PORT || 5000;
+const uri = process.env.MONGODB_URI;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 async function run() {
   try {
     await client.connect();
     const database = client.db("knowledgehub");
-    const userCollection =database.collection("user")
-    const sessionCollection = database.collection('session');
-    const bookcollection = database.collection("books")
-    const paymentCollection = database.collection("payments")
-
+    const userCollection = database.collection("user");
+    const sessionCollection = database.collection("session");
+    const bookcollection = database.collection("books");
+    const paymentCollection = database.collection("payments");
+    const reviewCollection = database.collection("reviews");
 
     //verify token
- const verifyToken =async (req, res, next) =>{
-  const authHeader = req.headers.authorization;
+    const verifyToken = async (req, res, next) => {
+      const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  
-  if(!token){
-   return res.status(401).json({ message: "Unauthorized access" }); 
-  }
-  const query = { token : token}
-  const session = await sessionCollection.findOne(query)
-  
-  const userId = session.userId
+      if (!authHeader) {
+        return res.status(401).json({ message: "Unauthorized access" });
+      }
+      const token = authHeader.split(" ")[1];
 
-  const userQuery={
-    _id : userId
-  }
-  const user = await userCollection.findOne(userQuery)
-  if (!user) {
-                return res.status(401).send({ message: 'unauthorized access' })
-            }
-            req.user = user;
-  next()
-}
+      if (!token) {
+        return res.status(401).json({ message: "Unauthorized access" });
+      }
+      const query = { token: token };
+      const session = await sessionCollection.findOne(query);
+
+      const userId = session.userId;
+
+      const userQuery = {
+        _id: userId,
+      };
+      const user = await userCollection.findOne(userQuery);
+      if (!user) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      req.user = user;
+      next();
+    };
 
     //book delevary releted query
     app.get("/api/delevary/user", verifyToken, async (req, res) => {
-  console.log("USER:", req.user);
+      console.log("USER:", req.user);
 
-  const userId = req.user._id.toString();
-  const query = {
-    userId,
-    delevaryStatus: "delevared",
-  };
-  const result = await paymentCollection.find(query).toArray();
-  res.send(result);
-});
+      const userId = req.user._id.toString();
+      const query = {
+        userId,
+        delevaryStatus: "delevared",
+      };
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
     //book delevary releted query
-    app.get("/api/delevary/librarian",verifyToken,async(req,res)=>{
-    const userId = req.user._id.toString();
-    
-    const result = await paymentCollection.find({userId}).toArray();
-    res.send(result || [])
-  
-    })
-    
+    app.get("/api/delevary/librarian", verifyToken, async (req, res) => {
+      const userId = req.user._id.toString();
+
+      const result = await paymentCollection.find({ userId }).toArray();
+      res.send(result || []);
+    });
+
     //payment releted
-    app.post("/api/payments",async(req,res)=>{
-      const history = req.body
-      const result = await paymentCollection.insertOne(history)
-      res.send(result)
-    })
+    app.post("/api/payments", async (req, res) => {
+      const history = req.body;
+      const result = await paymentCollection.insertOne(history);
+      res.send(result);
+    });
 
-    //Books releted 
-  app.post("/api/books",async(req,res)=>{
-  const book = req.body
-  const newBook ={
-    ...book,
-    createdAt : new Date()
-  }
-  const result = await bookcollection.insertOne(newBook)
-  res.send(result)
-})
+    //Books releted
+    app.post("/api/books", async (req, res) => {
+      const book = req.body;
+      const newBook = {
+        ...book,
+        createdAt: new Date(),
+      };
+      const result = await bookcollection.insertOne(newBook);
+      res.send(result);
+    });
 
-app.get("/api/books", async (req, res) => {
-  const query = {};
-  
-  if (req.query.librarianId) {
-    query.librarianId = req.query.librarianId;
-  }
-  
-  const result = await bookcollection.find(query).toArray();
-  res.send(result || []); 
-});
+    app.get("/api/books", async (req, res) => {
+      const query = {};
 
-//detels page
-  app.get("/api/book/:id",async(req,res)=>{
-    const id = req.params.id
-    const query ={
-      _id : new ObjectId(id)
-    }
-    const result = await bookcollection.findOne(query)
-    res.send(result)
-  })
-  
+      if (req.query.librarianId) {
+        query.librarianId = req.query.librarianId;
+      }
 
-//browse er jonno data get
-app.get("/api/all/books", async (req, res) => {
-    const query = {
-      approvalStatus: "approved",
-      publishStatus: "published",
-    };
+      const result = await bookcollection.find(query).toArray();
+      res.send(result || []);
+    });
 
-    if (req.query.search) {
-      query.$or = [
-        { title: { $regex: req.query.search, $options: "i" } },
-        { author: { $regex: req.query.search, $options: "i" } },
-        { category: { $regex: req.query.search, $options: "i" } },
-      ];
-    }
+    //detels page
+    app.get("/api/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await bookcollection.findOne(query);
+      res.send(result);
+    });
 
-    if (req.query.category && req.query.category !== "All") {
-      query.category = req.query.category;
-    }
-    if (req.query.page) {
+    //browse er jonno data get
+    app.get("/api/all/books", async (req, res) => {
+      const query = {
+        approvalStatus: "approved",
+        publishStatus: "published",
+      };
+
+      if (req.query.search) {
+        query.$or = [
+          { title: { $regex: req.query.search, $options: "i" } },
+          { author: { $regex: req.query.search, $options: "i" } },
+          { category: { $regex: req.query.search, $options: "i" } },
+        ];
+      }
+
+      if (req.query.category && req.query.category !== "All") {
+        query.category = req.query.category;
+      }
+      if (req.query.page) {
         const page = req.query.page;
         const perPage = req.query.perPage || 12;
-        const skipItems = (page - 1) * perPage
+        const skipItems = (page - 1) * perPage;
 
         const total = await bookcollection.countDocuments(query);
-        const cursor = bookcollection.find(query).skip(skipItems).limit(perPage);
+        const cursor = bookcollection
+          .find(query)
+          .skip(skipItems)
+          .limit(perPage);
         const books = await cursor.toArray();
         return res.send({ total, books });
-    }
+      }
 
-    const result = await bookcollection.find(query).toArray();
-    res.send(result || []); 
-  });
+      const result = await bookcollection.find(query).toArray();
+      res.send(result || []);
+    });
+
+    //review releted apis
+    app.post("/api/reviews", async (req, res) => {
+      const review = req.body;
+      const newReview = {
+        ...review,
+        createdAt: new Date(),
+      };
+      const result = await reviewCollection.insertOne(newReview);
+      res.send(result);
+    });
+
+    app.get("/api/reviews/:bookId", async (req, res) => {
+      const bookId = req.params.bookId;
+      const result = await reviewCollection.find({ bookId: bookId }).toArray();
+      res.send(result);
+    });
+
+    app.get("/api/my/reviews/:userId", async (req, res) => {
+      const userId = req.params.userId;
+      const result = await reviewCollection.find({userId : userId}).toArray();
+      res.send(result);
+    });
+
+    
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
